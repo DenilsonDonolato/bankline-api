22e3b33072ea8d7617e03c61844eef076dfb4133
Denilson Donolato <42578438+DenilsonDonolato@users.noreply.github.com>
2022-05-06 21:18:41 -0300

    Added search by id

--- src/main/java/com/dio/santander/banklineapi/repository/MovimentacaoRepository.java
+++ src/main/java/com/dio/santander/banklineapi/repository/MovimentacaoRepository.java
@@ -1,9 +1,12 @@
 package com.dio.santander.banklineapi.repository;
 
+import java.util.List;
+
 import com.dio.santander.banklineapi.model.Movimentacao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Integer> {
   
+  public List<Movimentacao> findByIdConta(Integer idConta);
 }
